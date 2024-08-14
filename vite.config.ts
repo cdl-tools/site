e4672a737c161e953e50b985077b6067b4fd8770
Katlyn Lorimer <katlyn@katlyn.dev>
2024-08-13 21:13:37 -0800

    refactor: rename pages dir to routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VueRouter(),
-    vue(),
+    VueRouter({
+      routesFolder: ['src/routes']
+    }),
+    vue()
   ],
   resolve: {
     alias: {
