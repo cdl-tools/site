084bd6b1bb0af4a40e22188963c1325001a5f8a5
Katlyn Lorimer <katlyn@katlyn.dev>
2024-09-13 19:45:26 -0800

    fix: use correct metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0",
+        },
+        {
+          property: "og:description",
+          content:
+            "Community Day League is a project set up by several Community Ambassadors as a way to try and generate engagement, friendly competition and awareness of Community Days. In short, it's a global catching competition.",
+        },
+        {
+          property: "og:title",
+          content: "Community Day League",
+        },
+        {
+          property: "og:image",
+          content: "~/assets/cdl-play-logo.png",
+        },
+      ],
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
